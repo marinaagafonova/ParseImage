7e0aaa290299ef9e092f1f1c8fe48d2ce4aeafd6
Ann Fobrales <fobrales@gmail.com>
2021-10-15 00:31:25 +0300

    make resizing images for dataset

--- detector/detector.py
+++ detector/detector.py
@@ -4,10 +4,14 @@ from wand.image import Image
 
 def resize_images():
     os.makedirs('resized', exist_ok=True)
+    count = 0;
     for file in os.listdir('science'):
         img = Image(filename='science/' + file)
-        img.transform(resize='200x')
-        img.save(filename = 'resized/' + file)
+        if (img.width > 200 or img.height > 200):
+            img.transform(resize='200x')
+            img.save(filename = 'resized/' + file)
+            count += 1
+    print('Resized ' + str(count) + ' images...')
 
 def detect():
     return random.choice(['diagram', 'graph', 'bar_chart', 'system', 'scheme', 'formula'])
