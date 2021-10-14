027f54dbaf2d47d372744e5538e7afa71ab3b5c9
Ann Fobrales <fobrales@gmail.com>
2021-10-15 01:29:32 +0300

    make resizing images for dataset

--- detector/detector.py
+++ detector/detector.py
@@ -7,7 +7,7 @@ def resize_images():
     count = 0;
     for file in os.listdir('science'):
         img = Image(filename='science/' + file)
-        if (img.width > 200 or img.height > 200):
+        if (img.width * img.height > 1048576):
             img.transform(resize='200x')
             img.save(filename = 'resized/' + file)
             count += 1
