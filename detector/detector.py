73f5002a7a62914997cdfbe53a7823f273c6f6ef
Ann Fobrales <fobrales@gmail.com>
2021-10-16 01:10:30 +0300

    keras preproccessing finaled

--- detector/detector.py
+++ detector/detector.py
@@ -6,45 +6,14 @@ import os
 import matplotlib.pyplot as plt 
 import PIL
 import tensorflow as tf
-import os
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
-
 import pandas as pd
 import requests # to get image from the web
 import shutil # to save it locally
 import time
 import numpy as np
-
-def map_to_numeric_values():
-    batch_size = 32
-    test_batch_size=37
-    img_height = 200
-    img_width = 200
-    data_dir = pathlib.Path("content/dataset/")
-    test_data_dir = pathlib.Path("content/test_dataset")
-    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
-        subset="training",
-        seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size, label_mode='binary')
-    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
-        subset="validation",
-        seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size,label_mode='binary')
-    test_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        test_data_dir,
-        seed=200,
-        image_size=(img_height, img_width),
-        batch_size=test_batch_size,label_mode='binary')
-    class_names = train_ds.class_names
-    print(class_names)
 
 def getFileNameFromUrl(url):
   firstpos=url.rindex("/")
@@ -89,34 +58,69 @@ def get_images_from_dataset(dataset_path, destinationFolder):
 
         downloadImage(row["Image URL"], destinationFolder)
 
+def map_to_numeric_values():
+    batch_size = 32
+    test_batch_size=37
+    img_height = 300
+    img_width = 300
+    data_dir = pathlib.Path("content/resized/dataset")
+    test_data_dir = pathlib.Path("content/resized/test_dataset")
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size, label_mode='binary')
+    class_names_train = train_ds.class_names
+    print("train classes " + class_names_train)
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size,label_mode='binary')
+    class_names_val = val_ds.class_names
+    print("val classes " + class_names_val)
+    test_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        test_data_dir,
+        seed=200,
+        image_size=(img_height, img_width),
+        batch_size=test_batch_size,label_mode='binary')
+    class_names_test = test_ds.class_names
+    print("test classes " + class_names_test)
+
+
+def resize_and_move(wo, wohin, maxsize = 90000):
+    count_resized = 0
+    count_moved = 0
+    for file in os.listdir(wo):
+        img = Image(filename = wo + "/" + file)
+        if (img.width * img.height > maxsize):
+            img.sample(300, 300)
+            count_resized += 1
+        img.save(filename = wohin + "/" + file)
+        count_moved += 1
+    print(str(count_moved) + " images was moved from " + wo + " to " + wohin + ". Resized " + str(count_resized) + " images.")
+    
+
 #пересохраняет изображение и, если размер изображения слишком большой, уменьшает его.
 def resize_images():
-    count = 0;
-    #обработка научных изображений
-    os.makedirs('resized', exist_ok=True)
-    count = 0
-    for file in os.listdir('content/dataset/article'):
-        img = Image(filename='content/dataset/article/' + file)
-        if (img.width * img.height > 1048576):
-            img.transform(resize='200x')
-            count += 1
-        img.save(filename = 'content/resized/dataset/1/' + file)
-    #обработка ненаучных изображений
-    for file in os.listdir('notscience'):
-        img = Image(filename='content/dataset/article/' + file)
-        if (img.width * img.height > 1048576):
-            img.transform(resize='200x')
-            count += 1
-        img.save(filename = 'content/resized/dataset/0/' + file)
-    # ресайз тестовой выборки данных???
-    print('Resized ' + str(count) + ' images...')
-    #map_to_numeric_values()
+    #обработка научных изображений из dataset, сохранение в директорию 1
+    resize_and_move('content/dataset/article', 'content/resized/dataset/1')
+    #обработка ненаучных изображений из dataset, сохранение в директорию 0
+    resize_and_move('content/dataset/others', 'content/resized/dataset/0')
+    #аналогичная обработка изображений из test_dataset
+    resize_and_move('content/test_dataset/article', 'content/resized/test_dataset/1')
+    resize_and_move('content/test_dataset/others', 'content/resized/test_dataset/0')
+    map_to_numeric_values()
     
 def dir_making():
-    os.makedirs('content/resized', exist_ok=True)
-    os.makedirs('content/resized/dataset', exist_ok=True)
     os.makedirs('content/resized/dataset/1', exist_ok=True)
     os.makedirs('content/resized/dataset/0', exist_ok=True)
+    os.makedirs('content/resized/test_dataset/1', exist_ok=True)
+    os.makedirs('content/resized/test_dataset/0', exist_ok=True)
 
 def dataset_proccessing():
     dir_making()
@@ -125,7 +129,6 @@ def dataset_proccessing():
 def detect():
     return random.choice(['diagram', 'graph', 'bar_chart', 'system', 'scheme', 'formula'])
     
-
 
 get_images_from_dataset('datasets/article_dataset.csv', "content/dataset/article")
 get_images_from_dataset('datasets/other_dataset.csv', "content/dataset/others")
